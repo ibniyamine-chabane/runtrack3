6368517be98396d11281f19baed8acb38198253d
ibniyamine-chabane <ibni-yamine.chabane@laplateforme.io>
2023-01-16 11:37:35 +0100

    Job05 ajouté et fait

---
+++ jour01/job05/script.js
@@ -0,0 +1,10 @@
+
+function afficherjoursemaines() {
+    const jourssemenaines = [ "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"];
+
+    for (let i = 0; i < 7; i++) {
+        console.log(jourssemenaines[i]);       
+    }
+}
+
+afficherjoursemaines();
