ca6bc3b1e2f49cf3b7f8d81805f1c05d6092fc51
ibniyamine-chabane <ibni-yamine.chabane@laplateforme.io>
2023-01-16 11:24:34 +0100

    Job03 et job04 fait

---
+++ jour01/job04/script.js
@@ -0,0 +1,11 @@
+
+function bisextile(annee) {
+
+    if ((annee % 4 === 0 && annee % 100 > 0) || (annee % 400 === 0)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bisextile(2024);
